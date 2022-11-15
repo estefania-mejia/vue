21f72014fa1a8cdae2b03efc743df6cf63b654e4
fany <44881951+estefania-mejia@users.noreply.github.com>
2022-11-15 13:48:55 -0600

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <CarComposition :costo="costo" />
+  
+</template>
+
+<script>
+import CarComposition from "./components/CarComposition";
+
+
+export default {
+  components:{
+    CarComposition,
+  },
+
+    setup() {
+      let costo = 30;
+
+      return{
+        costo,
+      };
+    },
+};
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
